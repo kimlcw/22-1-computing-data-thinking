5da84090bf1fd8e5c2f82f06f580ddfd40dae561
LCW <94828802+kimlcw@users.noreply.github.com>
2022-06-13 14:23:46 +0900

    Add files via upload

---
+++ 220613 실습.py
@@ -0,0 +1,22 @@
+
+# 파이썬에는 모듈이 있음. 그 중 하나가 turtle()
+import turtle
+
+# turtle.shape("turtle")
+turtle.pensize(5)
+turtle.pencolor("green")
+
+
+turtle.circle(100, 360, 3)
+turtle.clear()
+
+turtle.circle(100, 360, 4)
+turtle.clear()
+
+turtle.circle(100, 360, 5)
+turtle.clear()
+
+turtle.circle(100, 360, 6)
+turtle.clear()
+
+turtle.circle(100)
