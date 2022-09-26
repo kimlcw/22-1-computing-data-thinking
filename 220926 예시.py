ad0f9bf28a53d22f91f8ab504e76c4d1142d467b
LCW <94828802+kimlcw@users.noreply.github.com>
2022-09-26 14:25:50 +0900

    Add files via upload

---
+++ 220926 예시.py
@@ -0,0 +1,11 @@
+numbers = [22, 15, 33, 10, 3, 12, 5, 8]
+numbers[0] += 13
+numbers[1] -= 22
+numbers[2] *= 1.3
+numbers[3] /= 1.4
+numbers[4] **= 8
+numbers[5] %= 8
+numbers[6] //= 2
+numbers.pop() 
+
+print(numbers)
---
+++ 220926 혼자해보기.py
@@ -0,0 +1,18 @@
+num1 = 3
+num2 = 2.5
+letter1='상명대학교'
+letter2='서울캠퍼스'
+
+num1+num2
+num2-num2
+num1*num2
+num1/num2
+
+# string
+# 문자열의 덧셈 = 결합
+letter1+letter2
+# 문자열의 뺄셈 (X)
+# letter1-letter2
+
+# 정수형 - string형 (X)s
+# num1-letter
