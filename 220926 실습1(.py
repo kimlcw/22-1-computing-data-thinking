cc5ea4ecc9a680966bc9906c7c4ff46cfac780b5
LCW <94828802+kimlcw@users.noreply.github.com>
2022-09-26 14:33:34 +0900

    Add files via upload

---
+++ 220926 실습1(.py
@@ -0,0 +1,10 @@
+chulsu = ['한양아파트', 2, 88, 100, 74]
+younghee = ['단양빌라', 14, 100, 84, 74]
+
+print(chulsu[0] > younghee[0]) # True
+# 영어는 abc순, 한글은 가나다 순으로 뒤로 갈수록 큼. 'ㅎ', 'ㄷ'
+print(chulsu[1] < younghee[1])
+print(chulsu[2] >= younghee[2])
+print(chulsu[3] <= younghee[3])
+print(chulsu[4] != younghee[4])
+
