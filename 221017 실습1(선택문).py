bd9a989a35b4524d1a2a1c4bab59cdd5ace3f029
LCW <94828802+kimlcw@users.noreply.github.com>
2022-10-17 14:34:21 +0900

    Add files via upload

---
+++ 221017 실습1(선택문).py
@@ -0,0 +1,7 @@
+print("숫자를 입력받고 해당 숫자가 짝수라면 EVEN을, 홀수이면 ODD를 출력합니다.")
+num = int(input("숫자를 입력하세요 : "))
+
+if (num%2) == 0:
+    print("EVEN")
+else:
+    print("ODD")
---
+++ 221017 예제(선택문).py
@@ -0,0 +1,10 @@
+numroom = input("강의실 번호를 입력해주세요 : ")
+
+if "A" in numroom :
+    print("사범관 건물입니다!!!")
+elif "B" in numroom :
+    print("미술관 건물입니다!!!")
+elif "C" in numroom:
+    print("가정관 건물입니다!!!")
+else:
+    print("우리 학교 건물이 아닙니다!!!!!")
